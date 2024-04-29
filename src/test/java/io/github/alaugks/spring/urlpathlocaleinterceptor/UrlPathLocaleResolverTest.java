66ade3460fcbe1a5cb4911f0603a28d043561140
André Laugks <al@pb82.de>
2024-04-29 15:53:23 +0200

    Test defaultLocale

--- src/test/java/io/github/alaugks/spring/urlpathlocaleinterceptor/UrlPathLocaleResolverTest.java
+++ src/test/java/io/github/alaugks/spring/urlpathlocaleinterceptor/UrlPathLocaleResolverTest.java
@@ -1,6 +1,8 @@
 package io.github.alaugks.spring.urlpathlocaleinterceptor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Locale;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,35 +15,37 @@ class UrlPathLocaleResolverTest {
     private UrlPathLocaleResolver localResolver;
     private MockHttpServletRequest mockRequest;
     private MockHttpServletResponse mockResponse;
-    private CustomMockHttpServletRequest customMockRequest;
 
     @BeforeEach
     void beforeEach() {
         this.localResolver = new UrlPathLocaleResolver();
         this.mockRequest = new MockHttpServletRequest();
-        this.customMockRequest = new CustomMockHttpServletRequest();
-        this.customMockRequest.getLocale();
         this.mockResponse = new MockHttpServletResponse();
     }
 
     @Test
-    void test_resolveLocale_withoutLocaleOnNull() {
-        this.mockRequest.addPreferredLocale(Locale.forLanguageTag(""));
-        this.localResolver.setLocale(this.mockRequest, this.mockResponse, null);
-        assertEquals("", this.localResolver.resolveLocale(this.mockRequest).toString());
+    void test_resolveLocale_defaultLocale() {
+        MockHttpServletRequest mockHttpServletRequest = mock(MockHttpServletRequest.class);
+        when(mockHttpServletRequest.getLocale()).thenReturn(null);
+        this.localResolver.setDefaultLocale(Locale.forLanguageTag("de"));
+        this.localResolver.setLocale(mockHttpServletRequest, this.mockResponse, null);
+
+        assertEquals("de", this.localResolver.resolveLocale(mockHttpServletRequest).toString());
     }
 
     @Test
     void test_resolveLocale_withEmptyRequestLocale() {
         String userLanguage = System.getProperty("user.language");
-        this.localResolver.setLocale(this.customMockRequest, this.mockResponse, null);
+        this.localResolver.setLocale(new CustomMockHttpServletRequest(), this.mockResponse, null);
+
         assertEquals(userLanguage, this.localResolver.resolveLocale(this.mockRequest).toString());
     }
 
     @Test
     void test_resolveLocale_withLocale() {
         Locale locale = Locale.forLanguageTag("en-UK");
-        this.localResolver.setLocale(this.customMockRequest, this.mockResponse, locale);
+        this.localResolver.setLocale(new CustomMockHttpServletRequest(), this.mockResponse, locale);
+
         assertEquals("en_UK", this.localResolver.resolveLocale(this.mockRequest).toString());
     }
 }
